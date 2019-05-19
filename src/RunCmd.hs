1a55cebd95f5cb765fa0f24bc05a96c7406f3e0c
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-05-19 02:06:29 -0400

    run simple command

---
+++ src/RunCmd.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module RunCmd(runCmd) where
+
+import RIO
+import qualified RIO.Text as T
+import qualified System.Process.Typed as P
+
+import Lang.Types
+
+runCmd :: Env -> ExecCmd -> IO UUID
+runCmd env cmd = do
+    (res, _) <- P.readProcess_ process
+    let resstr = T.unpack (decodeUtf8Lenient (toStrictBytes res))
+    case readMaybe resstr of
+        Just uuid -> return (UUID uuid)
+        _ -> throwString "No valid UUID returned!"
+    where
+        (subcmd, cmdstr) = buildCmd cmd
+        envstr = buildEnv env
+        process = P.proc "cl" (concat [[subcmd], envstr, [cmdstr]])
+
+
+buildCmd :: ExecCmd -> (String, String)
+buildCmd (Bash es) = ("run", T.unpack (T.intercalate " " (displayEle <$> es)))
+    where
+        displayEle (Verbatim e) = e
+        displayEle (Val e) = e
+
+buildEnv :: Env -> [String]
+buildEnv es = envStr <$> es
+    where
+        envStr (n, e) = T.unpack (T.concat [n, ":", e])
