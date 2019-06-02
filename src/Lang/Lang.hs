54692f89ecdf0d205e70fec44218763a9afeadec
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-06-02 16:11:37 -0400

    add export point for lang

---
+++ src/Lang/Lang.hs
@@ -0,0 +1,18 @@
+module Lang.Lang
+    ( module Lang.Interpret
+    , module Lang.Parser
+    , module Lang.RCO
+    , module Lang.Types
+    , module Lang.TypeCheck
+    , module Lang.PPrint
+    , module Lang.Fold
+    )
+where
+
+import           Lang.Interpret
+import           Lang.Parser
+import           Lang.RCO
+import           Lang.Types
+import           Lang.TypeCheck
+import           Lang.PPrint
+import           Lang.Fold
