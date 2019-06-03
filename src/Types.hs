6966ebbb974890037d16f95af4ca6d5e25c0e7a2
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-06-03 02:29:40 -0400

    add log info during execution

--- src/Types.hs
+++ src/Types.hs
@@ -3,10 +3,12 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Types where
 
 import           RIO                     hiding ( view )
+import qualified RIO.Text as T
 import           RIO.Process
 import qualified RIO.Map                       as M
 import           Control.Lens
@@ -34,12 +36,32 @@ instance HasLogFunc App where
 instance HasProcessContext App where
   processContextL = appProcessContext
 
+data LogEnt = EntVerbatim Text | EntUUID Text | EntParen LogEnt
+  deriving (Show, Read, Eq)
+data LogInfo = Assign [LogEnt] [LogEnt]
+  deriving (Show, Read, Eq)
+
+instance Display LogInfo where
+  textDisplay (Assign v vs) = T.concat [renderList v, " <- ", renderList vs]
+    where
+      renderList as = T.intercalate " " (render <$> as)
+      render (EntVerbatim t) = t
+      render (EntUUID u) = T.take 7 u
+      render (EntParen e) = T.concat ["(", render e, ")"]
+
+appLog :: LogInfo -> RIO App ()
+appLog l = logInfo (display l)
 
 instance Exec (RIO App) Text where
-  clLit _ u = return (tshow u)
-  clRun _ depMap cmd = do
+  clLit vn u = do
+    let ustr = tshow u
+    appLog (Assign [EntVerbatim vn] [EntUUID ustr])
+    return ustr
+  clRun vn depMap cmd = do
     clcmd <- view appClCmd
-    tshow <$> liftIO (clcmd execCmd)
+    resid <- tshow <$> liftIO (clcmd execCmd)
+    appLog (Assign [EntVerbatim vn, EntParen (EntUUID resid)] (EntVerbatim <$> cmdTxt))
+    return resid
    where
     fromEle e = case e of
       Plain t        -> t
